3aabd64d9004aa5374d75e671597a99ca8716a1a
Rômulo Evangelista <57496953+romulop40@users.noreply.github.com>
2022-05-03 09:32:51 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,271 @@
+					<?php
+					require "helper/config.php";
+					?>
+					<!DOCTYPE html>
+					<html lang="pt-br">
+					<head>
+						<meta charset="utf-8" />
+						<link rel="shortcut icon" href="img/JonesOliveira.ico" type="PNG/logo" />
+						<meta http-equiv="Content-type" content="text/html; charset=utf-8">
+					    <meta http-equiv="Cache-control" content="no-cache">
+					    <meta http-equiv="Pragma" content="no-cache">
+					    <meta http-equiv="Expires" content="0">
+					    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+					<meta charset="utf-8">
+					<meta http-equiv="X-UA-Compatible" content="IE=edge">
+					<meta name="viewport" content="width=device-width, initial-scale=1">
+					<title><?php echo $CustomerName ?></title>
+					<!-- Default stylesheets-->
+					<link href="assets/lib/bootstrap/dist/css/bootstrap.min.css"
+						rel="stylesheet">
+					<!-- Template specific stylesheets-->
+					<link
+						href="https://fonts.googleapis.com/css?family=Roboto+Condensed:400,700"
+						rel="stylesheet">
+					<link href="https://fonts.googleapis.com/css?family=Volkhov:400i"
+						rel="stylesheet">
+					<link
+						href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700,800"
+						rel="stylesheet">
+					<link href="assets/lib/animate.css/animate.css" rel="stylesheet">
+					<link href="assets/lib/components-font-awesome/css/font-awesome.min.css"
+						rel="stylesheet">
+					<link href="assets/lib/et-line-font/et-line-font.css" rel="stylesheet">
+					<link href="assets/lib/flexslider/flexslider.css" rel="stylesheet">
+					<link href="assets/lib/owl.carousel/dist/assets/owl.carousel.min.css"
+						rel="stylesheet">
+					<link
+						href="assets/lib/owl.carousel/dist/assets/owl.theme.default.min.css"
+						rel="stylesheet">
+					<link href="assets/lib/magnific-popup/dist/magnific-popup.css"
+						rel="stylesheet">
+					<link href="assets/lib/simple-text-rotator/simpletextrotator.css"
+						rel="stylesheet">
+					<!-- Main stylesheet and color file-->
+					<link href="assets/css/style.css" rel="stylesheet">
+					<link id="color-scheme" href="assets/css/colors/default.css"
+						rel="stylesheet">
+					<!--<meta name="viewport" content="width=device-width, initial-scale=1">
+					  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+					  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+					  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>-->
+				    <link rel="stylesheet" href="login.css" type="text/css" />
+				    </head>
+					<body data-spy="scroll" data-target=".onpage-navigation"
+						data-offset="60">
+						<main>
+						<div class="page-loader">
+							<div class="loader">Loading...</div>
+						</div>
+						<nav class="navbar navbar-custom navbar-fixed-top navbar-transparent"
+							role="navigation">
+							<div class="container">
+								<div class="navbar-header">
+									<button class="navbar-toggle" type="button" data-toggle="collapse"
+										data-target="#custom-collapse">
+										<span class="sr-only"></span><span
+											class="icon-bar"></span><span class="icon-bar"></span><span
+											class="icon-bar"></span>
+									</button>
+									<!--Imagem Leayout-->
+									<a class="navbar-brand" href="./" style="padding: 7px 7px;">
+										<!--<img src="helper/<?php echo $CustomerIkonnCode ?>_200.png" width="100"></a>-->
+										<img src="img/JonesOliveira.png" width="200"></a>
+								</div>
+
+
+								<div class="collapse navbar-collapse" id="custom-collapse">
+									<ul class="nav navbar-nav navbar-right">
+										<li><a class="section-scroll" href="#">Jones advogados associados</a></li>
+										<li><a class="section-scroll" href="#software">\Acesso\</a></li>
+				    <script>
+					// Get the modal
+					var modal = document.getElementById('id01');
+
+					// When the user clicks anywhere outside of the modal, close it
+					window.onclick = function(event) {
+					    if (event.target == modal) {
+					        modal.style.display = "none";
+					    }
+					}
+					</script>
+
+					</body>
+					</html>
+					       </ul>
+								</div>
+							</div>
+						</nav>
+						<section class="home-section bg-dark-30" id="home"
+							data-background="img/background.jpg">
+							<!-- 		<div class="video-player" -->
+							<!-- 			data-property="{videoURL:'https://www.youtube.com/watch?v=uzlUiiH9IFo', containment:'.home-section', startAt:30, mute:false, autoPlay:true, loop:true, opacity:1, showControls:false, showYTLogo:false, vol:25}"></div> -->
+							<!-- 		</div> -->
+							<div class="titan-caption">
+								<div class="caption-content">
+									<div class="font-alt mb-30 titan-title-size-1">Bem vindo à</div>
+									<div class="font-alt mb-40 titan-title-size-4"><?php echo $CustomerName ?></div>
+									<!--<a class="section-scroll btn btn-border-w btn-round" href="#contact">Entre
+										em Contato</a>-->
+								</div>
+							</div>
+						</section>
+						
+							<section class="module-small" id="software">
+								<div class="container">
+									<div class="row">
+										<div class="col-sm-6 col-sm-offset-3">
+											<h2 class="module-title font-alt">Acesso Rapido ao Escritorio</h2>
+
+											<h2 class="module-subtitle font-alt"><?php echo $CustomerName ?></h2>
+										</div>
+									<br></div>
+
+									<div class="row multi-columns-row post-columns" >
+										<div class="col-sm-6 col-md-4 col-lg-4">
+											<div class="post mb-20">
+												<div class="post-thumbnail">
+
+												<a href="Painel/index.php"><img src="img/sistema.png" alt="Blog-post Thumbnail"/><a/>
+
+												</div>
+												<div class="post-header font-alt">
+													<h2 class="post-title">Acesso ao Sistema</h2>
+												</div>
+												<div class="post-entry">
+
+													<p>Sistema Advocacia</p>
+
+												</div>
+											</div>
+										</div>
+										<div class="col-sm-6 col-md-4 col-lg-4">
+											<div class="post mb-20">
+												<div class="post-thumbnail">
+
+													
+													<a href=""><img src="img/usuario.png" alt="Blog-post Thumbnail" /><a/>
+												  
+												</div>
+												<div class="post-header font-alt">
+													<h2 class="post-title">Cliente</h2>
+												</div>
+												<div class="post-entry">
+													
+													<p>Sistemas Clientes</p>
+												  
+												</div>
+											</div>
+										</div>
+										<div class="col-sm-6 col-md-4 col-lg-4" >
+											<div class="post mb-20" >
+												<div class="post-thumbnail">
+
+													<a href=""><img src="img/pasta.png" alt="Blog-post Thumbnail" /><a/>
+
+												</div>
+												<div class="post-header font-alt">
+													<h2 class="post-title">Acervo</h2>
+												</div>
+												<div class="post-entry">
+
+													<p>Documentos e Petições</p>
+
+												</div>
+											</div>
+										</div>
+									</div>
+									
+							</section>
+							
+							<div class="module-small bg-dark">
+								<div class="container">
+									<div class="row">
+										<div class="col-sm-4">
+											<div class="widget">
+												<h5 class="widget-title font-alt"><?php echo $CustomerName?></h5>
+							                      <?php
+												if ($CustomerPhones) {
+											    foreach ( $CustomerPhones as $phone ) {
+												echo "Fone: " . $phone . "<br/>";
+										        }
+										        }
+											      ?>
+							                      <?php echo ($CustomerContactEmail ? "Email: <a href=\"mailto:Desenvolvido por REFByte".$CustomerContactEmail."\">". $CustomerContactEmail."</a><br/>" : "")?>
+							                      <?php echo ($CustomerAddr1 ? "Endereço: ".$CustomerAddr1 : "")?>
+											</div>
+										</div>
+										<div class="col-sm-4">
+											<div class="widget">
+												<h5 class="widget-title font-alt">Sobre Nós</h5>
+												<p>
+													<a class="section-scroll" href="#about"><?php echo $CustomerName ?>
+												</a>
+												</p>
+											</div>
+										</div>
+										<div class="col-sm-4">
+											<div class="widget">
+												<h5 class="widget-title font-alt">Download</h5>
+												<ul class="widget-posts">
+													<li class="clearfix">Disponível para Clientes</li>
+													<li class="clearfix">
+														<div class="widget-posts-image"
+															style="width: 125px; padding-right: 10px;">
+															<a
+																href="https://link"
+																target="_blank"><img src="img/googleplay.png"
+																alt="Android" style="border-radius: 3px;" /></a>
+														</div>
+														<!--<div class="widget-posts-image"
+															style="width: 125px; padding-right: 10px;">
+															<a
+																href="https://link"
+																target="_blank"><img src="images/appstore.png" alt="iOS"
+																style="border-radius: 3px;" /></a>--->
+														</div>
+													</li>
+												</ul>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+							<hr class="divider-d">
+							<footer class="footer bg-dark">
+								<div class="container">
+									<div class="row">
+										<div class="col-sm-6">
+											<p class="copyright font-alt">
+												<a target="_blank" href="">Desenvolvido por REFByte &#169</a>
+											</p>
+										</div>
+										<div class="col-sm-6"></div>
+									</div>
+								</div>
+							</footer>
+						</div>
+						<div class="scroll-up">
+							<a href="#totop"><i class="fa fa-angle-double-up"></i></a>
+						</div>
+						</main>
+						<!--  
+					    JavaScripts
+					    =============================================
+					    -->
+						<script src="assets/lib/jquery/dist/jquery.js"></script>
+						<script src="assets/lib/bootstrap/dist/js/bootstrap.min.js"></script>
+						<script src="assets/lib/wow/dist/wow.js"></script>
+						<script src="assets/lib/jquery.mb.ytplayer/dist/jquery.mb.YTPlayer.js"></script>
+						<script src="assets/lib/isotope/dist/isotope.pkgd.js"></script>
+						<script src="assets/lib/imagesloaded/imagesloaded.pkgd.js"></script>
+						<script src="assets/lib/flexslider/jquery.flexslider.js"></script>
+						<script src="assets/lib/owl.carousel/dist/owl.carousel.min.js"></script>
+						<script src="assets/lib/smoothscroll.js"></script>
+						<script src="assets/lib/magnific-popup/dist/jquery.magnific-popup.js"></script>
+						<script
+							src="assets/lib/simple-text-rotator/jquery.simple-text-rotator.min.js"></script>
+						<script src="assets/js/plugins.js"></script>
+						<script src="assets/js/main.js"></script>
+					</body>
+					</html>
